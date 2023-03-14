7d4a6c460b8653afb792ed1cb79b6bddaadc2c23
Saqlain <96402850+saqlain024@users.noreply.github.com>
2023-03-14 19:40:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-# TODO-List-Using-API
-Buillding a todo list using API
-how its look like
-![Screenshot (1322)](https://user-images.githubusercontent.com/96402850/225026846-9c25e878-6a71-484c-b906-349a006e2524.png)
-![Screenshot (1323)](https://user-images.githubusercontent.com/96402850/225026867-a350b10c-8ad6-47a4-9ed9-c8ff63721d83.png)
-![Screenshot (1324)](https://user-images.githubusercontent.com/96402850/225026899-73ec470e-66e2-4ec9-8fd9-f2366c812bc0.png)
-![Screenshot (1325)](https://user-images.githubusercontent.com/96402850/225026910-ee95106f-3498-4c5d-86db-c8ae6788282c.png)
+# Todo list app
+A todo list app
+
+
+### General steps to follow when creating a project
+
+- Thinking about the UI
+- Functionality
+	- Add a TODO
+	- Delete a TODO
+	- check task
+	- total items count
+- Data
+	- tasks - an array
+	- task - {done, text, id}
+- Functions (in code)
+	- addTodo
+	- deleteTodo
+	- checkTodo
+	- renderTodosList
+	- showNotification
---
+++ todo.js
@@ -0,0 +1,178 @@
+(function () {
+
+    let tasks = [];
+    const tasksList = document.getElementById('list');
+    const addTaskInput = document.getElementById('add');
+    const tasksCounter = document.getElementById('tasks-counter');
+    
+    var a = 10;
+    
+    // async function fetchTodos () {
+    //     // GET request
+    // //  fetch('https://jsonplaceholder.typicode.com/todos')  //return a promise
+    // //     .then(function(response) {
+    // //         // console.log(response);              //chaining 
+    // //         return response.json();             //this (json) also return promise
+    // //     }).then(function (data) {
+    // //         // console.log(data);
+    // //         tasks = data.slice(0,10);
+    // //         renderList();
+    // //     })
+    // //     .catch( function (error) {
+    // //         console.log('error', error);
+    // //     })
+    
+    //       // doing same thing using async and await 
+    //       try {
+    
+    //         const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    //         const data = await response.json();
+    //         tasks =  data.slice(0,10);
+    //         renderList();
+    
+    //       } catch (error) {
+    //         console.log(error)
+    //       }
+        
+    
+    // }
+    
+    function addTasktoDOM (task) {
+        const li = document.createElement('li');
+    
+        li.innerHTML = `
+            <input type="checkbox" id="${task.id}" ${task.completed ? 'checked' : ''} class="custom-checkbox">
+            <label for="${task.id}">${task.title}</label>
+            <img src="delete-trash.svg" class="delete" data-id="${task.id}" />
+        `;
+    
+        tasksList.append(li);
+    
+    }
+    
+    function renderList () {
+        tasksList.innerHTML = '';
+    
+        for (let i = 0; i < tasks.length; i++) {
+            addTasktoDOM(tasks[i]);
+        }
+    
+        tasksCounter.innerHTML = tasks.length; 
+    }
+    
+    function toggletask (taskId) { 
+        const task = tasks.filter( function (task) {
+            return task.id === Number(taskId);   //===
+        })
+    
+        if (task.length > 0 ) {
+            const currentTask = task[0];
+    
+            currentTask.completed = !currentTask.completed; //
+            renderList();
+            showNotification('task toggle successfully');
+            return;
+        }
+    
+        showNotification('could not toggle the task');
+    
+    }
+    
+    function deleteTask (taskId) {
+        const newTasks = tasks.filter( function (task) {
+            return task.id !== Number(taskId);   //!==
+        })
+    
+        tasks = newTasks;
+        renderList();
+        showNotification('task deleted successfully');
+    }
+    
+    function addTask (task) {
+        
+        if (task) {
+            // fetch('https://jsonplaceholder.typicode.com/todos', {
+            //     method: POST,
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify(task),
+    
+            // }).then(function(response) {
+            //     // console.log(response);              //chaining 
+            //     return response.json();             //this (json) also return promise
+            // }).then(function (data) {
+            //     // tasks = data.slice(0,10);
+            //     renderList();
+            //     tasks.push(task);
+            //     renderList();    
+            //     showNotification('Task added successfully'); 
+            // })
+            // .catch( function (error) {
+            //     console.log('error', error);
+            // })
+    
+            tasks.push(task);
+            renderList();    
+            showNotification('Task added successfully');
+            return;
+        }
+    
+        showNotification('Task can not be added');
+    }
+    
+    function showNotification(text) {
+        alert(text);
+    }
+    
+    function handleInputKeypress(e) {
+        if (e.key == 'Enter') {
+            const text = e.target.value;
+            console.log('text', text);  //comment
+    
+            if (!text) {
+                showNotification('Task text can not be reply');
+                return;
+            }
+    
+            const task = { 
+                title: text,              // this is same as this line ->  text : text
+                // id: Date.now().toString(),
+                id: Date.now(), 
+                done: false
+            }
+    
+            e.target.value = '';  // making input box empty again
+            addTask(task);      // function calling to add task
+        }
+    
+    }
+    
+    
+    function handleClickListener (e) {
+        const target = e.target;
+        // console.log(target);
+    
+        if ( target.className === 'delete') {
+            const taskId = target.dataset.id;
+            deleteTask(taskId);
+            return; 
+        }
+        else if (target.className === 'custom-checkbox') { // ===
+            const taskId = target.id;           //  notice this -> not  - target.dataset.id;
+            toggletask(taskId);
+            return;
+        }  
+    
+    }
+    
+    function initializeApp () {
+        // fetchTodos();
+        addTaskInput.addEventListener('keyup',handleInputKeypress);
+        document.addEventListener('click', handleClickListener)    
+    }
+    
+    initializeApp();
+    
+})();
+
